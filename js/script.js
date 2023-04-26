a64fd7bc8b1983c655bea7bc17aecf89319057ed
Dominik Domański <dominikd33@o2.pl>
2023-04-26 08:24:47 +0200

    add object

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
-  optArticleTagsSelector = '.post-tags .list';
+  optArticleTagsSelector = '.post-tags .list',
+  optTagsListSelector = 'tags.list';
 {
   const titleClickHandler = function(event){
     event.preventDefault();
@@ -72,9 +73,11 @@ const optArticleSelector = '.post',
   }
 
   generateTitleLinks();
-}
-{
+
   function generateTags(){
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
+
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
 
@@ -102,6 +105,16 @@ const optArticleSelector = '.post',
         /* add generated code to html variable */
         html = html + linkHTML;
 
+        /* [NEW] check if this link is NOT already in allTags */
+        if(!allTags[tag]){
+
+          /* [NEW] add tag to allTags object */
+          allTags[tag] = 1;
+          } else {
+            allTags[tag]++;
+        }
+
+
       /* END LOOP: for each tag */
       }
       /* insert HTML of all the links into the tags wrapper */
@@ -109,8 +122,23 @@ const optArticleSelector = '.post',
 
     /* END LOOP: for every article: */
     }
-  }
 
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML + linkHTML;
+  }
   generateTags();
 
 
@@ -168,4 +196,5 @@ const optArticleSelector = '.post',
   }
 
   addClickListenersToTags();
+
 }
